6738b3e0579c51647e2e584d4df5c80d5ea42d39
BennoAlif <bennoalif41@gmail.com>
2020-10-27 22:41:00 +0700

    Create register servoce

--- go.mod
+++ go.mod
@@ -10,10 +10,10 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/ugorji/go v1.1.13 // indirect
-	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897
 	golang.org/x/sys v0.0.0-20201024232916-9f70ab9862d5 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 	gorm.io/driver/mysql v1.0.3 // indirect
-	gorm.io/gorm v1.20.5 // indirect
+	gorm.io/gorm v1.20.5
 )
--- main.go
+++ main.go
@@ -4,37 +4,11 @@ import (
 	"bwastartup/user"
 	"log"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-
-	// fmt.Println("Koneksi berhasil")
-
-	// var users []user.User
-
-	// db.Find(&users)
-
-	// for _, user := range users {
-	// 	fmt.Println(user.Name)
-	// 	fmt.Println(user.Email)
-	// 	fmt.Println("----------------")
-	// }
-
-	router := gin.Default()
-	router.GET("/handler", handler)
-	router.Run()
-}
-
-func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -42,10 +16,15 @@ func handler(c *gin.Context) {
 		log.Fatal(err.Error())
 	}
 
-	var users []user.User
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
 
-	db.Find(&users)
+	userInput := user.RegisterUserInput{}
+	userInput.Name = "Tes simpan"
+	userInput.Email = "hohohihe@gmail.com"
+	userInput.Occupation = "Chef"
+	userInput.Password = "test"
 
-	c.JSON(200, users)
+	userService.RegisterUser(userInput)
 
 }
